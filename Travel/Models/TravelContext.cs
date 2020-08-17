2540c7abed464e3cab5632ec327ce0d99cd08418
Beverly Potts <pottsbeverly@gmail.com>
2020-08-17 13:05:12 -0400

    Add initial files
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

---
+++ Travel/Models/TravelContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Travel.Models
+{
+  public class TravelContext : DbContext
+  {
+    public TravelContext(DbContextOptions<TravelContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Review> Reviews { get; set; }
+  }
+}
